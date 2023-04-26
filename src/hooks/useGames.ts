98e86ce14b5f27979a31c1300f82093c433d20f9
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-26 21:13:44 +0530

    Refactor - separating entity interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,10 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
 import ms from "ms";
 import useGameStore from "../store";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  description_raw: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-}
-
 const useGames = () => {
   const gameQuery = useGameStore((s) => s.gameQuery);
   return useInfiniteQuery<FetchResponse<Game>, Error>({
