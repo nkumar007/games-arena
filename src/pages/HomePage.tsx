48d8966e2bfbce39905088c3d98fd8ba72d6834b
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-26 14:55:04 +0530

    setting up routing with react-router6

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,42 @@
+import { Grid, Show, GridItem, Flex, Box } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GenresList from "../components/GenresList";
+import HeadingComponent from "../components/HeadingComponent";
+import PlatformSelector from "../components/PlatformSelector";
+import SortByOrder from "../components/SortByOrder";
+
+const HomePage = () => {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: ` " main"`,
+          lg: ` "aside main"`, // screens wider and above 1024
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <Show above="lg">
+          <GridItem area={"aside"} paddingX="20px">
+            <GenresList />
+          </GridItem>
+        </Show>
+        <GridItem area={"main"}>
+          <Box paddingLeft={3}>
+            <HeadingComponent />
+            <Flex gap={3} marginBottom={2}>
+              <PlatformSelector />
+              <SortByOrder />
+            </Flex>
+          </Box>
+
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
+
+export default HomePage;
