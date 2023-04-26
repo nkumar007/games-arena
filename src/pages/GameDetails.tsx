71ed0f5e57fe3add042344a5043f35a852875990
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-26 21:34:58 +0530

    build expandable text

--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -1,6 +1,7 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
 const GameDetails = () => {
@@ -14,7 +15,7 @@ const GameDetails = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
