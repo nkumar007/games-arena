17eb1029a905f9e92fc6cef3a0d0b2ce05ec8cd3
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-27 18:28:16 +0530

    Refactor: fixing the exports

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import getCroppedImageUrl from "../services/optimize-image";
 import CriticsRating from "./CriticsRating";
 import PlatformIconList from "./PlatformIconList";
